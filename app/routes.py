97a47218eec90c6ec901ab62fb458ede8c9fef2d
DerFacn <derfacn.pro228@gmail.com>
2023-08-22 16:40:06 +0200

    routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 def index():
-    return 'Hello, World'
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('account/login.html', title='Login')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('account/signup.html', title='Sign Up')
